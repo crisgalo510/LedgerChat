183c63c2c5183903d64cde161c31574ecb8dc336
Cristhian Galo <cristhiangalo@MacBook-Air-27.local>
2026-02-24 21:55:56 -0800

    admin.py, registered Transaction in admin

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Transaction
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("user", "transaction_type", "category", "amount", "date", "created_at")
+    list_filter = ("transaction_type", "category", "date")
+    search_fields = ("user__username", "description")
+    ordering = ("-date", "-created_at")
